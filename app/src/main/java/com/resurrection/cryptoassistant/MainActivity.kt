88708f848ffc633209b4a3643b94f405afa51e42
Yusuf Okur <0yusufokur0@gmail.com>
2021-09-17 09:39:03 +0300

    added view binding

--- app/src/main/java/com/resurrection/cryptoassistant/MainActivity.kt
+++ app/src/main/java/com/resurrection/cryptoassistant/MainActivity.kt
@@ -2,10 +2,13 @@ package com.resurrection.cryptoassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.resurrection.cryptoassistant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view = binding.root
+        setContentView(view)
     }
 }
