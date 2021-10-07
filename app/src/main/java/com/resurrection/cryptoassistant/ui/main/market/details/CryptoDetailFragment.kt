3b84602a5a939512961bebd6425240414fca1fad
yusuf <0yusufokur0>
2021-10-07 11:42:50 +0300

    insert favorite crypto in crypto detail fragment transfered to crypto detail viewmodel

--- app/src/main/java/com/resurrection/cryptoassistant/ui/main/market/details/CryptoDetailFragment.kt
+++ app/src/main/java/com/resurrection/cryptoassistant/ui/main/market/details/CryptoDetailFragment.kt
@@ -10,11 +10,9 @@ import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.resurrection.cryptoassistant.R
 import com.resurrection.cryptoassistant.data.model.CryptoDetailItem
-import com.resurrection.cryptoassistant.data.model.CryptoMarketModel
 import com.resurrection.cryptoassistant.data.model.FavouriteCryptoModel
 import com.resurrection.cryptoassistant.databinding.BottomSheetFragmentBinding
 import com.resurrection.cryptoassistant.ui.base.BaseBottomSheetFragment
@@ -26,73 +24,38 @@ class CryptoDetailFragment(private val mContext: Context) :
     BaseBottomSheetFragment<BottomSheetFragmentBinding>() {
     var cryptoDetailItem: CryptoDetailItem? = null
     private val viewModel: CryptoDetailViewModel by viewModels()
+    var isFavorite:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme) }
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme)
+    }
 
     override fun init(savedInstanceState: Bundle?) {
         getDetail()
         binding.favoriteImageView.setBackgroundColor(Color.RED)
         binding.favoriteImageView.setOnClickListener {
-            val user = Firebase.auth.currentUser
-            if (user != null) {
-
-                val test = FavouriteCryptoModel(
-                    binding.cryptoDetail!!.id,
-                    binding.cryptoDetail!!.marketData.currentPrice.usd.toString(),
-                    binding.cryptoDetail!!.lastUpdated
-                )
-
-                val database = Firebase.database
-                val myRef = database.getReference(user.uid).child("favorite")
-                    .child(binding.cryptoDetail!!.id).setValue(test)
-                    .addOnSuccessListener {
-                        var currentCrypto = binding.cryptoDetail!!
-                        var cryptomarketModel = CryptoMarketModel(
-                            currentCrypto.id,
-                            currentCrypto.marketData.currentPrice.usd,
-                            currentCrypto.marketData.highestPrice24h.usd,
-                            currentCrypto.image.small,
-                            currentCrypto.lastUpdated,
-                            currentCrypto.marketData.lowestPrice24h.usd,
-                            currentCrypto.name,
-                            currentCrypto.marketData.priceChange24h,
-                            currentCrypto.marketData.priceChangePercentage24h,
-                            currentCrypto.symbol
-                        )
-                        viewModel.insertFavoriteCrypto(cryptomarketModel)
-                    }.addOnFailureListener{
-
-                    }
-
-            } else {
-                // No user is signed in
-            }
-
-
+            if (!isFavorite){
+                viewModel.insertFavoriteCrypto(binding.cryptoDetail!!,binding.favoriteImageView)
+            }else{ /*this crypto is favorite */ }
         }
 
         viewModel.isFavorite.observe(viewLifecycleOwner, Observer {
-            if (it!!){
+            if (it!!) {
                 binding.favoriteImageView.setBackgroundColor(Color.GREEN)
-            }else{
+                isFavorite = true
+            } else {
                 binding.favoriteImageView.setBackgroundColor(Color.RED)
+                isFavorite = false
             }
             println(it)
         })
 
     }
 
-
-    override fun getLayoutRes(): Int {
-        return R.layout.bottom_sheet_fragment
-    }
-
     fun getDetail() {
         val data = arguments?.getString("cryptoId")
         viewModel.isFavorite(data!!)
-
         binding.progressbar.visibility = View.VISIBLE
 
         viewModel.getCryptoDetailById(data.toString())
@@ -102,42 +65,29 @@ class CryptoDetailFragment(private val mContext: Context) :
             println(it.image.small.toString())
             Glide.with(requireContext()).load(it.image.large).into(binding.imgIconImage)
             binding.progressbar.visibility = View.INVISIBLE
-
-
-
         })
     }
-/*
-    fun isFavorite(){
 
-        val user = Firebase.auth.currentUser
-        if (user != null) {
-            val db = FirebaseFirestore.getInstance()
-            db.collection(user.uid).get()
-                .addOnSuccessListener { documentReference ->
-
-                    documentReference.documents.forEach {
-                        if (it.get("id") as String == binding.cryptoDetail!!.id){
-                            binding.favoriteImageView.setBackgroundColor(Color.GREEN)
-                        }else{
-                            binding.favoriteImageView.setBackgroundColor(Color.RED)
-                        }
-                    }
-                }
-                .addOnFailureListener { e ->
-
-                }
-
-        } else {
-            // No user is signed in
-        }
+    override fun getLayoutRes(): Int {
+        return R.layout.bottom_sheet_fragment
     }
-*/
-
-
 
 }
 
+
+/*var currentCrypto = binding.cryptoDetail!!
+var cryptomarketModel = CryptoMarketModel(
+    currentCrypto.id,
+    currentCrypto.marketData.currentPrice.usd,
+    currentCrypto.marketData.highestPrice24h.usd,
+    currentCrypto.image.small,
+    currentCrypto.lastUpdated,
+    currentCrypto.marketData.lowestPrice24h.usd,
+    currentCrypto.name,
+    currentCrypto.marketData.priceChange24h,
+    currentCrypto.marketData.priceChangePercentage24h,
+    currentCrypto.symbol
+)*/
 
 
 
--- app/src/main/java/com/resurrection/cryptoassistant/ui/main/market/details/CryptoDetailViewModel.kt
+++ app/src/main/java/com/resurrection/cryptoassistant/ui/main/market/details/CryptoDetailViewModel.kt
@@ -1,70 +1,74 @@
 package com.resurrection.cryptoassistant.ui.main.market.details
 
 import android.app.Application
+import android.graphics.Color
+import android.widget.ImageView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.resurrection.cryptoassistant.data.RepositoryTest
 import com.resurrection.cryptoassistant.data.db.CryptoDatabase
 import com.resurrection.cryptoassistant.data.model.CryptoDetailItem
-import com.resurrection.cryptoassistant.data.model.CryptoMarketModel
+import com.resurrection.cryptoassistant.data.model.FavouriteCryptoModel
 import com.resurrection.cryptoassistant.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import okhttp3.Dispatcher
 import javax.inject.Inject
-import com.google.firebase.firestore.DocumentSnapshot
-
-import com.google.firebase.firestore.QuerySnapshot
-
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.database.ktx.database
-import com.resurrection.cryptoassistant.data.model.FavouriteCryptoModel
 
 
 @HiltViewModel
 class CryptoDetailViewModel @Inject constructor(application: Application) :
     BaseViewModel(application) {
-    var job : Job? = null
+    var job: Job? = null
     var cryptoDetail = MutableLiveData<CryptoDetailItem>()
     val dao = CryptoDatabase(getApplication()).cryptoDao()
     var isFavorite = MutableLiveData<Boolean?>()
     var app = application
 
 
-    fun getCryptoDetailById(id:String){
+    fun getCryptoDetailById(id: String) {
         job = CoroutineScope(Dispatchers.IO).launch {
             var temp = RepositoryTest(app).api.getCryptoById(id)
             cryptoDetail.postValue(temp)
         }
     }
 
-    fun insertFavoriteCrypto(cmm: CryptoMarketModel)= viewModelScope.launch{
-        dao.insertFavoriteCrypto(cmm)
-    }
+    fun insertFavoriteCrypto(cryptoDetail: CryptoDetailItem, favoriteImageView: ImageView) =
+        viewModelScope.launch {
+            val user = Firebase.auth.currentUser
+            if (user != null) {
+                val test = FavouriteCryptoModel(
+                    cryptoDetail.id,
+                    cryptoDetail.marketData.currentPrice.usd.toString(),
+                    cryptoDetail.lastUpdated
+                )
+                val database = Firebase.database
+                val myRef = database.getReference(user.uid).child("favorite")
+                    .child(cryptoDetail.id).setValue(test)
+                    .addOnSuccessListener {
+                        favoriteImageView.setBackgroundColor(Color.GREEN)
+                    }.addOnFailureListener { }
+            } else { /*No user is signed in */
+            }
 
+        }
 
 
     fun isFavorite(checkId: String) {
-        /*
-        * Böyle bir id li crypto varmı kontrol edilecek
-        *
-        * */
-
         val user = Firebase.auth.currentUser
         if (user != null) {
             val database = Firebase.database
             val myRef = database.getReference(user.uid).child("favorite")
                 .child(checkId).get().addOnSuccessListener {
-                    if (it.value == null){
+                    if (it.value == null) {
                         isFavorite.value = false
                         println(it)
-                    }else{
+                    } else {
                         isFavorite.value = true
                     }
 
@@ -74,14 +78,11 @@ class CryptoDetailViewModel @Inject constructor(application: Application) :
                 }
 
         } else { // No user is signed in }
+        }
     }
 
-
-
-}
     override fun onCleared() {
         super.onCleared()
         job = null
-
     }
-    }
+}
